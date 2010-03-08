8ef050a501e559480b3c44df813b4717a5ba28c6
mcclanahoochie <mcclanahoochie@8a095940-2a22-11df-b279-ed2be448802f>
2010-03-08 21:48:22 +0000

    adding initial java bluetooth files

---
+++ sprime-bt/src/org/java/bluetooth/sprime/BluetoothServiceDiscovery.java
@@ -0,0 +1,150 @@
+package org.java.bluetooth.sprime;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Vector;
+
+import javax.bluetooth.DeviceClass;
+import javax.bluetooth.DiscoveryAgent;
+import javax.bluetooth.DiscoveryListener;
+import javax.bluetooth.LocalDevice;
+import javax.bluetooth.RemoteDevice;
+import javax.bluetooth.ServiceRecord;
+import javax.bluetooth.UUID;
+
+/**
+ * 
+ * Class that discovers all bluetooth devices in the neighborhood,
+ * 
+ * Connects to the chosen device and checks for the presence of OBEX push service in it. and
+ * displays their name and bluetooth address.
+ * 
+ * 
+ */
+public class BluetoothServiceDiscovery implements DiscoveryListener {
+
+	// object used for waiting
+	private static Object lock = new Object();
+
+	// vector containing the devices discovered
+	private static Vector<RemoteDevice> vecDevices = new Vector<RemoteDevice>();
+
+	// device connection address
+	private static String connectionURL = null;
+
+	/**
+	 * Lists nearby bluetooth device services
+	 */
+	public void listServices() throws IOException {
+
+		// display local device address and name
+		LocalDevice localDevice = LocalDevice.getLocalDevice();
+		System.out.println("Address: " + localDevice.getBluetoothAddress());
+		System.out.println("Name: " + localDevice.getFriendlyName());
+
+		// find devices
+		DiscoveryAgent agent = localDevice.getDiscoveryAgent();
+		System.out.println("Starting device inquiry...");
+		agent.startInquiry(DiscoveryAgent.GIAC, this);
+
+		// avoid callback conflicts
+		try {
+			synchronized (lock) {
+				lock.wait();
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Device Inquiry Completed. ");
+
+		// print all devices in vecDevices
+		int deviceCount = vecDevices.size();
+		if (deviceCount <= 0) {
+			System.out.println("No Devices Found .");
+		} else {
+			// print bluetooth device addresses and names in the format [ No. address (name) ]
+			System.out.println("Bluetooth Devices: ");
+			for (int i = 0; i < deviceCount; i++) {
+				RemoteDevice remoteDevice = (RemoteDevice) vecDevices.elementAt(i);
+				try {
+					System.out.println((i + 1) + ". " + remoteDevice.getBluetoothAddress() + " (" + remoteDevice.getFriendlyName(true) + ")");
+				} catch (IOException ioe) {
+					ioe.printStackTrace();
+				}
+			}
+		}
+
+		// prompt user
+		System.out.print("Choose the device to search for Obex Push service : ");
+		BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
+		String chosenIndex = bReader.readLine();
+		int index = Integer.parseInt(chosenIndex.trim());
+
+		// check for obex service
+		RemoteDevice remoteDevice = (RemoteDevice) vecDevices.elementAt(index - 1);
+		UUID[] uuidSet = new UUID[1];
+		uuidSet[0] = new UUID("1105", true);
+		System.out.println("\nSearching for service...");
+		agent.searchServices(null, uuidSet, remoteDevice, this);
+
+		// avoid callback conflicts
+		try {
+			synchronized (lock) {
+				lock.wait();
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		// notify
+		if (connectionURL == null) {
+			System.out.println("Device does not support Object Push.");
+		} else {
+			System.out.println("Device supports Object Push.");
+		}
+	}
+
+	/**
+	 * Called when a bluetooth device is discovered. Used for device search.
+	 */
+	public void deviceDiscovered(RemoteDevice btDevice, DeviceClass cod) {
+		// add the device to the vector
+		if (!vecDevices.contains(btDevice)) {
+			vecDevices.addElement(btDevice);
+		}
+	}
+
+	/**
+	 * Called when a bluetooth service is discovered. Used for service search.
+	 */
+	public void servicesDiscovered(int transID, ServiceRecord[] servRecord) {
+		if (servRecord != null && servRecord.length > 0) {
+			connectionURL = servRecord[0].getConnectionURL(0, false);
+		}
+		synchronized (lock) {
+			lock.notify();
+		}
+	}
+
+	/**
+	 * Called when the service search is over.
+	 */
+	public void serviceSearchCompleted(int transID, int respCode) {
+		synchronized (lock) {
+			lock.notify();
+		}
+	}
+
+	/**
+	 * Called when the device search is over.
+	 */
+	public void inquiryCompleted(int discType) {
+		synchronized (lock) {
+			lock.notify();
+		}
+
+	}// end method
+
+}// end class
---
+++ sprime-bt/src/org/java/bluetooth/sprime/SprimeBT.java
@@ -0,0 +1,50 @@
+package org.java.bluetooth.sprime;
+
+import java.io.IOException;
+
+public class SprimeBT {
+
+	/**
+	 * Main
+	 * 
+	 * @param args
+	 */
+
+	// main method of the application
+	public static void main(String[] args) throws IOException {
+
+		/*
+		 * Test bed for BT classes. Set to true/false for testing
+		 */
+
+		if (false) {
+			// scan and print nearby bluetooth devices
+			System.out.println("\n" + "Testing BT Device Discovery" + "\n");
+			BluetoothDeviceDiscovery bluetoothDeviceDiscovery = new BluetoothDeviceDiscovery();
+			bluetoothDeviceDiscovery.listDevices();
+		}
+
+		if (false) {
+			// scan and print nearby bluetooth device services
+			System.out.println("\n" + "Testing BT Service Discovery" + "\n");
+			BluetoothServiceDiscovery bluetoothServiceDiscovery = new BluetoothServiceDiscovery();
+			bluetoothServiceDiscovery.listServices();
+		}
+
+		if (false) {
+			// start a bluetooth server
+			System.out.println("\n" + "Testing BT Server" + "\n");
+			SimpleSPPServer sppServer = new SimpleSPPServer();
+			sppServer.runServer();
+		}
+
+		if (false) {
+			// start a bluetooth client
+			System.out.println("\n" + "Testing BT Client" + "\n");
+			SimpleSPPClient sppClient = new SimpleSPPClient();
+			sppClient.runClient();
+		}
+
+	}// end main
+
+}// end class
