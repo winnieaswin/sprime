495f53c986e2348568bd7d166950bb2c85de580f
mcclanahoochie <mcclanahoochie@8a095940-2a22-11df-b279-ed2be448802f>
2010-04-09 20:44:03 +0000

    updated graphing and sensors

--- sprimeserver/src/groovy/sprime/server/Receiving.groovy
+++ sprimeserver/src/groovy/sprime/server/Receiving.groovy
@@ -12,47 +12,53 @@ import sprime.server.PowerUsage;
 
 public class Receiving implements Runnable {
 
-    private double k = 0.33;
-
-    private double avgDisplay = 0;
+    private double k = 0.75;		// percent of new value to believe
+	private double min_zero = 507;	// don't dip
+    private double avgDisplay = 507;// start at about zero
     
     private StreamConnection connection = null;
     private InputStream inStream = null;
-
     private boolean stop = false;
 
     public Receiving(StreamConnection c) {
         connection = c;
-	try {
-            inStream = connection.openInputStream();
-	} catch (final Exception e) {
-            e.printStackTrace();
-	}
+		try {
+		        inStream = connection.openInputStream();
+		} catch (final Exception e) {
+		        e.printStackTrace();
+		}
     }
 
     @Override
     public void run() {
         while (!this.stop) {
             try {
-
-		BufferedReader bReader = new BufferedReader(new InputStreamReader(inStream));
-		String lineRead = bReader.readLine();
-		System.out.println("Server recv: " + lineRead);
+				BufferedReader bReader = new BufferedReader(new InputStreamReader(inStream));
+				String lineRead = bReader.readLine();
+				System.out.println("Server recv: " + lineRead);
 
                 int watt = Integer.parseInt(lineRead);
-                avgDisplay = (watt * k) + avgDisplay * (1 - k);
-
-                PowerUsage usage = new PowerUsage(wattage: avgDisplay);
+                if(watt>=min_zero){
+                	avgDisplay = (watt * k) + avgDisplay * (1 - k);
+                }
+				
+				// ~10 W per arduino division @5v sensor input
+				double val = (avgDisplay-min_zero) ;
+				if(val<0) val = 0;
+				val = val * 10;
+				
+				// display graph
+                PowerUsage usage = new PowerUsage(wattage: val);
                 usage.save();
-
-		Thread.sleep(1000);
+				Thread.sleep(666);
             } catch (final Exception e) {
                 e.printStackTrace();
             }
-	}
+		}
     }
 
     public void stop() {
         this.stop = true;
     }
 } // class
+
